670622fc9f09e00b189017d402d1f4ec1b92e8a6
carlosesilva116 <carlos.esilva116@gmail.com>
2025-08-15 18:56:20 -0300

    Add files via upload

---
+++ do-while/(1).cs
@@ -0,0 +1,15 @@
+﻿//Criar um programa que solicite senha até que o 
+//usuário digite a correta, usando do-while em C#.
+
+string senha = "1234";
+string senhadig;
+do
+{
+    Console.WriteLine("Digite sua senha:");
+    senhadig = (Console.ReadLine());
+    if (senhadig != senha)
+    {
+        Console.WriteLine("Senha incorreta, tente novamente!");
+    }
+} while (senhadig != senha);
+Console.WriteLine("Acesso liberado!");
