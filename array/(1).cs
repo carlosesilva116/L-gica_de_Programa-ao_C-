9aa2784fc6a32af196103815e74b3a91ee4a0502
carlosesilva116 <carlos.esilva116@gmail.com>
2025-08-15 19:07:56 -0300

    Add files via upload

---
+++ array/(1).cs
@@ -0,0 +1,25 @@
+﻿//Criei um vetor para armazenar nomes de 5 alunos.
+//Criar outro vetor para armazenar médias desses alunos.
+//no final, exibir cada aluno com sua respectiva média.
+
+
+string[] alunos = new string[5];
+
+for (int i = 0; i < alunos.Length; i++)
+{
+    Console.WriteLine($"Digite o nome do aluno {i + 1} :");
+    alunos[i] = Convert.ToString(Console.ReadLine());
+}
+Console.Clear();
+
+Double[] media = new double[5];
+
+for (int i = 0; i < media.Length; i++)
+{
+    Console.WriteLine($"Digite a média do aluno{i + 1} :");
+    media[i] = Convert.ToDouble(Console.ReadLine());
+}
+for (int i = 0; i < media.Length; i++)
+{
+    Console.WriteLine($"{alunos[i]} sua média é: {media[i]}");
+}
