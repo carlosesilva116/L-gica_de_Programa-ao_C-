ece74a924bcd93d58078c5a0a65d46bcdba34481
carlosesilva116 <carlos.esilva116@gmail.com>
2025-08-15 18:51:05 -0300

    Add files via upload

---
+++ while/(1).cs
@@ -0,0 +1,19 @@
+﻿//Criar um programa que receba as idades digitadas até que o usuário digite uma negativa
+//ao final, mostrar a média das idades digitadas.
+
+int cont = -1;
+double idade = 0;
+double soma = 0;
+
+while (idade >= 0)
+{
+    soma += idade;
+  
+    Console.WriteLine("Digite uma idade para acumular: ");
+    idade = Convert.ToDouble(Console.ReadLine());
+    cont++;
+}
+
+double media = soma / cont;
+
+Console.WriteLine($"A média das idades é de: {media}");
