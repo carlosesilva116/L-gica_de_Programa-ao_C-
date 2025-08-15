eaaaac286c06cd0b15d8defd81c18c36f967d7cd
carlosesilva116 <carlos.esilva116@gmail.com>
2025-08-15 18:58:44 -0300

    Add files via upload

---
+++ for/(1).cs
@@ -0,0 +1,11 @@
+﻿// Crie uma taboada com o número escolhido pelo usuário:
+
+Console.WriteLine("Digite um número:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"você escolheu a taboada do: {num}");
+
+for (int cont = 1; cont <= 10; cont++)
+{
+    Console.WriteLine($"{num} X {cont} = {num * cont}");
+}
